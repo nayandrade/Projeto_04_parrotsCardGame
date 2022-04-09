807bd8bbba54fd773ac2da1945d0f6ac1e609a5f
Nayara <nrdeandrade@gmail.com>
2022-04-09 14:18:20 -0300

    requisitos básicos de forma grosseira

--- scripts.js
+++ scripts.js
@@ -9,7 +9,14 @@ const img7 = "img/img7.gif";
 const imagens = [img1, img2, img3, img4, img5, img6, img7]
 let imagensEmbaralhadas = []
 let cartasDistribuir = []
-
+let hasFlippedCard = false;
+let primeiraCarta;
+let segundaCarta;
+let acerto = 0
+let rodadas = 0
+let click = 0
+document.querySelector("h2").innerHTML = acerto;
+document.querySelector("h3").innerHTML = click;
 let card = prompt("Com quantas cartas vossa senhoria deseja jogar? Pares, de 4 a 14")
 
 while (card % 2 !== 0) {
@@ -24,34 +31,34 @@ function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
-      
+
     }
-  }
+}
 
 distribuirCartas()
 function distribuirCartas() {
     shuffle(imagens);
-    for (let i = 0; i < (card/2) ; i++) {
+    for (let i = 0; i < (card / 2); i++) {
         imagensEmbaralhadas.push(imagens[i])
     }
-    
+
     duplicarCartas()
 }
 
 
 function duplicarCartas() {
-    for (let i = 0; i < imagensEmbaralhadas.length; i++) {        
-        cartasDistribuir.push(imagensEmbaralhadas[i])             
+    for (let i = 0; i < imagensEmbaralhadas.length; i++) {
+        cartasDistribuir.push(imagensEmbaralhadas[i])
     }
-    for (let j = 0; j < imagensEmbaralhadas.length; j++) {        
-        cartasDistribuir.push(imagensEmbaralhadas[j])             
-    }    
-    shuffle(cartasDistribuir) 
-    adicionarCarta()    
+    for (let j = 0; j < imagensEmbaralhadas.length; j++) {
+        cartasDistribuir.push(imagensEmbaralhadas[j])
+    }
+    shuffle(cartasDistribuir)
+    adicionarCarta()
 }
 
 function adicionarCarta() {
-    
+
     for (let i = 0; i < card; i++) {
         let lista = document.querySelector(".container")
         lista.innerHTML += `
@@ -63,10 +70,81 @@ function adicionarCarta() {
 }
 
 function escolherCarta(elemento) {
+    elemento.classList.toggle("flip");
+    elemento.children[0].classList.toggle("escondido");
+    elemento.children[1].classList.toggle("escondido");
+
+    if (hasFlippedCard === false) {
+        hasFlippedCard = true;
+        primeiraCarta = elemento
+        console.log(primeiraCarta)
+    } else {
+        segundaCarta = elemento;
+        hasFlippedCard = false;
+        console.log(segundaCarta)
+        checar();
+    }
+
+    click += 1
+    document.querySelector("h3").innerHTML = click;
+    
+}
+
+function checar() {
+    let primeirona = primeiraCarta.children[1].src
+    let segundona = segundaCarta.children[1].src
+    console.log(primeirona)
+    console.log(segundona)
+
+    if (primeirona == segundona) {
+        //alert("aeee")
+        primeirona = ""
+        segundona = ""
+        disableCards();
+        resetar()
+    } else {
+        //alert("xi")
+        unflipCards();
+    }
 
 }
 
+function disableCards() {
+    primeiraCarta = ""
+    segundaCarta = ""
+    acerto += 1
+    document.querySelector("h2").innerHTML = acerto;
+}
 
+function unflipCards() {
+
+    setTimeout(() => {
+        primeiraCarta.classList.remove("flip")
+        primeiraCarta.children[0].classList.remove("escondido");
+        primeiraCarta.children[1].classList.add("escondido");
+        segundaCarta.classList.remove("flip")
+        segundaCarta.children[0].classList.remove("escondido");
+        segundaCarta.children[1].classList.add("escondido");
+        //rodadas += 1
+        document.querySelector("h3").innerHTML = rodadas;
+       
+    }, 1000);
+
+    //primeiraCarta.classList.remove("flip")
+    //primeiraCarta.children[0].classList.remove("escondido");
+    //primeiraCarta.children[1].classList.add("escondido");
+    //segundaCarta.classList.remove("flip")
+    //segundaCarta.children[0].classList.remove("escondido");
+    //segundaCarta.children[1].classList.add("escondido");
+    //rodadas += 1
+    //document.querySelector("h3").innerHTML = rodadas;
+}
+
+function resetar() {
+    if(acerto === card/2) {
+        alert(`Você ganhou em ${click + 1} jogadas!`)
+    }
+}
 
 
 

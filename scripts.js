37c13653705e0a33b4146b8416fd7df9ec90f2d8
Nayara <nrdeandrade@gmail.com>
2022-04-09 18:01:27 -0300

    Requisitos Básicos

--- scripts.js
+++ scripts.js
@@ -15,36 +15,29 @@ let segundaCarta;
 let acerto = 0
 let rodadas = 0
 let click = 0
-document.querySelector("h2").innerHTML = acerto;
-document.querySelector("h3").innerHTML = click;
-let card = prompt("Com quantas cartas vossa senhoria deseja jogar? Pares, de 4 a 14")
+let card = prompt("Com quantas cartas vossa senhoria deseja jogar? Pares, de 4 a 14");
+let tempo = false;
 
-while (card % 2 !== 0) {
-    card = prompt("Com quantas cartas vossa senhoria deseja jogar? Pares, de 4 a 14")
-
-    while (card < 4 || card > 14) {
-        card = prompt("Com quantas cartas vossa senhoria deseja jogar? Pares, de 4 a 14")
-    }
+while (card % 2 !== 0 || card < 4 || card > 14) {
+    card = prompt("Com quantas cartas vossa senhoria deseja jogar? Pares, de 4 a 14")    
 }
 
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
-
     }
 }
 
-distribuirCartas()
 function distribuirCartas() {
     shuffle(imagens);
     for (let i = 0; i < (card / 2); i++) {
         imagensEmbaralhadas.push(imagens[i])
     }
-
     duplicarCartas()
 }
 
+distribuirCartas()
 
 function duplicarCartas() {
     for (let i = 0; i < imagensEmbaralhadas.length; i++) {
@@ -58,21 +51,24 @@ function duplicarCartas() {
 }
 
 function adicionarCarta() {
-
     for (let i = 0; i < card; i++) {
         let lista = document.querySelector(".container")
         lista.innerHTML += `
-        <div class="card" onclick="escolherCarta(this)">
-        <img class="card-front" src="${img0}" alt="">
-        <img class="card-back escondido" src="${cartasDistribuir[i]}" alt="">
+        <div class="card" ">
+        <img class="card-front face" onclick="escolherCarta(this)" src="${img0}" alt="">
+        <img class="card-back face escondido" src="${cartasDistribuir[i]}" alt="">
     </div>`
     }
 }
 
 function escolherCarta(elemento) {
-    elemento.classList.toggle("flip");
-    elemento.children[0].classList.toggle("escondido");
-    elemento.children[1].classList.toggle("escondido");
+
+    if (tempo === false) {
+        elemento.parentNode.classList.add("flip");
+    elemento.classList.add("escondido");
+    setTimeout(() => {
+        elemento.nextElementSibling.classList.remove("escondido");
+    }, 100);
 
     if (hasFlippedCard === false) {
         hasFlippedCard = true;
@@ -85,64 +81,60 @@ function escolherCarta(elemento) {
         checar();
     }
 
-    click += 1
-    document.querySelector("h3").innerHTML = click;
+    click += 1   
+    console.log(click)
+
+    }
     
 }
 
 function checar() {
-    let primeirona = primeiraCarta.children[1].src
-    let segundona = segundaCarta.children[1].src
+    let primeirona = primeiraCarta.nextElementSibling.src
+    let segundona = segundaCarta.nextElementSibling.src
     console.log(primeirona)
     console.log(segundona)
 
     if (primeirona == segundona) {
-        //alert("aeee")
+        
         primeirona = ""
         segundona = ""
         disableCards();
         resetar()
     } else {
-        //alert("xi")
+        
         unflipCards();
     }
-
 }
 
 function disableCards() {
     primeiraCarta = ""
     segundaCarta = ""
-    acerto += 1
-    document.querySelector("h2").innerHTML = acerto;
+    acerto += 1    
+    console.log(acerto)
 }
 
 function unflipCards() {
-
+    tempo = true
+    console.log(tempo)
     setTimeout(() => {
-        primeiraCarta.classList.remove("flip")
-        primeiraCarta.children[0].classList.remove("escondido");
-        primeiraCarta.children[1].classList.add("escondido");
-        segundaCarta.classList.remove("flip")
-        segundaCarta.children[0].classList.remove("escondido");
-        segundaCarta.children[1].classList.add("escondido");
-        //rodadas += 1
-        document.querySelector("h3").innerHTML = rodadas;
-       
+        primeiraCarta.parentNode.classList.remove("flip")
+        primeiraCarta.classList.remove("escondido");
+        primeiraCarta.nextElementSibling.classList.add("escondido");
+        segundaCarta.parentNode.classList.remove("flip")
+        segundaCarta.classList.remove("escondido");
+        segundaCarta.nextElementSibling.classList.add("escondido");
+        tempo = false;
+        console.log(tempo)
+        
     }, 1000);
-
-    //primeiraCarta.classList.remove("flip")
-    //primeiraCarta.children[0].classList.remove("escondido");
-    //primeiraCarta.children[1].classList.add("escondido");
-    //segundaCarta.classList.remove("flip")
-    //segundaCarta.children[0].classList.remove("escondido");
-    //segundaCarta.children[1].classList.add("escondido");
-    //rodadas += 1
-    //document.querySelector("h3").innerHTML = rodadas;
+    console.log(tempo)    
 }
 
 function resetar() {
-    if(acerto === card/2) {
-        alert(`Você ganhou em ${click + 1} jogadas!`)
+    if (acerto === card / 2) {
+        setTimeout(() => {
+            alert(`Você ganhou em ${click} jogadas!`)
+        }, 110);
     }
 }
 
